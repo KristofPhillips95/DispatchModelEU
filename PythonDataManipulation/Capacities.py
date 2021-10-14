70df0c3cb089a63bdf43f3073be55a6551d4c6c7
Kristof Phillips <Kristof.phillips@kuleuven.be>
2021-10-14 10:23:03 +0200

    Technology mapping, line sets

--- PythonDataManipulation/Capacities.py
+++ PythonDataManipulation/Capacities.py
@@ -1,12 +1,48 @@
 import pandas as pd
-import os
+import csv
 
+import os
+#Read input data from main file
 df_gen = pd.read_excel(io = "../Input Data/TYNDP-2020-Scenario-Datafile.xlsx",engine = 'openpyxl',sheet_name= "Capacity" )
 df_lines = pd.read_excel(io = "../Input Data/TYNDP-2020-Scenario-Datafile.xlsx",engine = 'openpyxl',sheet_name= "Line" )
 
-nodes = set(df_gen["Node/Line"])
-generators = set(df_gen["Generator_ID"])
+#Rename column in the generator dataframe
+df_gen = df_gen.rename(columns={"Node/Line" : "Node"})
+
+####
+#Creating file with all generator ideas present in dataset, and their total capacities
+#####
+#generators = set(df_gen["Generator_ID"])
+#gen_cap_dict = {generator : sum(df_gen[ (df_gen["Generator_ID"] == generator) & (df_gen["Year"] == 2040) ].Value)/1000 for generator in generators}
+#df_generator_id = pd.DataFrame.from_dict(gen_cap_dict, orient= 'index')
+#df_generator_id.to_csv("../Input Data/list_of_generators.csv")
+#######
+
+df_gen_dict = pd.read_excel(io = "../Input Data/Mapping_generators.xlsx",engine = 'openpyxl',sheet_name= "Mapping" )
+
+gen_dict = dict(zip(df_gen_dict.Generator_ID,df_gen_dict.Mapped_to))
+df_gen_mapped = df_gen.replace(gen_dict)
+df_gen_mapped = df_gen_mapped[df_gen_mapped["Generator_ID"]!= "NI"]
+
+
+#Create sets of nodes, lines, and generators
+nodes_all = set(df_gen["Node"])
+nodes = set(df_gen_mapped["Node"])
 connections = set(df_lines["Node/Line"])
 
-df_gen.to_csv("../Input Data/gen_cap.csv")
-df_lines.to_csv("../Input Data/lines.csv")
+
+df= df_lines['Node/Line'].apply(lambda x: pd.Series(x.split('-')))
+df = df.rename(columns={0: "Node1",1: "Node2"})
+df_lines["Node1"] = df.Node1
+df_lines["Node2"] = df.Node2
+
+
+df_lines = df_lines[(df_lines["Node1"].isin(nodes_all)) & (df_lines["Node2"].isin(nodes_all))]
+df_lines_mapped = df_lines[(df_lines["Node1"].isin(nodes)) & (df_lines["Node2"].isin(nodes))]
+
+df_gen.to_csv("../Input Data/gen_cap_all.csv")
+df_gen_mapped.to_csv("../Input Data/gen_cap.csv")
+
+df_lines.to_csv("../Input Data/lines_all.csv")
+df_lines_mapped.to_csv("../Input Data/lines.csv")
+
--- model_builder.jl
+++ model_builder.jl
@@ -2,27 +2,184 @@
 using Pkg
 Pkg.activate(@__DIR__) # @__DIR__ = directory this script is in
 Pkg.instantiate()
+using JuMP
+using DataFrames
+using CSV
+
 ##
 
 
-function define_sets(m::Model,scenario::String,year::Int,CY::Int)
+function define_sets!(m::Model,scenario::String,year::Int,CY::Int)
+
     m.ext[:sets] = Dict()
     m.ext[:sets][:technologies] = Dict()
+    m.ext[:sets][:dispatchable_technologies] = Dict()
+    m.ext[:sets][:intermittent_technologies] = Dict()
+    m.ext[:sets][:connections] = Dict()
+
 
     reading = CSV.read("Input Data\\gen_cap.csv",DataFrame)
     reading = reading[reading[!,"Scenario"] .== scenario,:]
     reading = reading[reading[!,"Year"] .== year,:]
     reading = reading[reading[!,"Climate Year"] .== CY,:]
 
-    m.ext[:sets][:countries] = [country for country in Set(reading[!,"Node/Line"])]
+    m.ext[:sets][:countries] = [country for country in Set(reading[!,"Node"])]
 
     for country in m.ext[:sets][:countries]
-        technologies = Set(reading[reading[!,"Node/Line"] .== country,:Generator_ID])
+        technologies = Set(reading[reading[!,"Node"] .== country,:Generator_ID])
         m.ext[:sets][:technologies][country] = [tech for tech in technologies]
+        m.ext[:sets][:dispatchable_technologies][country] = filter(tech -> (tech != "w_on") && (tech != "PV") && (tech != "w_off"),m.ext[:sets][:technologies][country])
+    end
+
+    reading_lines = CSV.read("Input Data\\lines.csv",DataFrame)
+    reading_lines = reading_lines[reading_lines[!,"Scenario"] .== scenario,:]
+    reading_lines = reading_lines[reading_lines[!,"Climate Year"] .== CY,:]
+    reading_lines = reading_lines[reading_lines[!,"Year"] .== year,:]
+
+    m.ext[:sets][:connections] = Dict(country => [] for country in m.ext[:sets][:countries])
+    for country in m.ext[:sets][:countries]
+        print(country)
+        reading_lines_country = reading_lines[reading_lines[!,"Node1"] .== country,:]
+        for other_country in reading_lines_country.Node2
+            println(other_country)
+            if !(other_country in m.ext[:sets][:connections][country])
+                m.ext[:sets][:connections][country] = vcat(m.ext[:sets][:connections][country],other_country)
+            end
+            if !(country in m.ext[:sets][:connections][other_country])
+                m.ext[:sets][:connections][other_country] = vcat(m.ext[:sets][:connections][other_country],country)
+            end
+        end
     end
 end
 
+function process_parameters!(m::Model,scenario::String,year::Int,CY::Int)
+    countries = m.ext[:sets][:countries]
+    technologies = m.ext[:sets][:technologies]
+    connections = m.ext[:sets][:connections]
+
+    m.ext[:parameters] = Dict()
+
+    m.ext[:parameters][:technologies] = Dict()
+    m.ext[:parameters][:connections] = Dict()
+
+
+    reading = CSV.read("Input Data\\gen_cap.csv",DataFrame)
+    reading = reading[reading[!,"Scenario"] .== scenario,:]
+    reading = reading[reading[!,"Year"] .== year,:]
+    reading = reading[reading[!,"Climate Year"] .== CY,:]
+
+    for country in countries
+        m.ext[:parameters][:technologies][country] = Dict()
+        reading_country = reading[reading[!,"Node"] .== country,:]
+        println(country)
+        for technology in technologies[country]
+            print(technology)
+            capacity = reading_country[reading_country[!,"Generator_ID"] .== technology,:].Value
+            #@assert(length(capacity) == 1)
+            m.ext[:parameters][:technologies][country][technology] = sum(capacity)
+        end
+    end
+
+    reading_lines = CSV.read("Input Data\\lines.csv",DataFrame)
+    reading_lines = reading_lines[reading_lines[!,"Scenario"] .== scenario,:]
+    reading_lines = reading_lines[reading_lines[!,"Climate Year"] .== CY,:]
+    reading_lines = reading_lines[reading_lines[!,"Year"] .== year,:]
+
+    #Initialize dicts
+    for country in countries
+        m.ext[:parameters][:connections][country] = Dict()
+    end
+    # Extract line capacities from data file
+    for country in Set(reading_lines.Node1)
+        reading_country = reading_lines[(reading_lines[!,"Node1"] .== country),:]
+        for node_2 in Set(reading_country.Node2)
+            capacity_imp = reading_country[(reading_country[!,"Node2"] .== node_2).&(reading_country[!,"Parameter"] .== "Import Capacity"),:].Value
+            if (country == "BE00" && node_2 == "DE00")
+                @show(reading_country[(reading_country[!,"Node2"] .== node_2).&(reading_country[!,"Parameter"] .== "Import Capacity"),:])
+            end
+            m.ext[:parameters][:connections][country][node_2] = abs.(capacity_imp)
+            capacity_exp = reading_country[(reading_country[!,"Node2"] .== node_2).&(reading_country[!,"Parameter"] .== "Export Capacity"),:].Value
+            m.ext[:parameters][:connections][node_2][country] = abs.(capacity_exp)
+        end
+        # reading_country = reading_lines[(reading_lines[!,"Node2"] .== country),:]
+        # for node_2 in connections[country]
+        #     capacity = reading_country[reading_country[!,"Node1"] .== node_2,:].Value
+        #     m.ext[:parameters][:connections][country][node_2] = capacity
+        # end
+    end
+    # Post check on line parameters to fill missing values to 0
+    for node1 in keys(m.ext[:sets][:connections])
+        #@show(node1)
+        for node2 in m.ext[:sets][:connections][node1]
+            #print(node2)
+            #@assert(!(isempty(m.ext[:parameters][:connections][node1][node2])))
+            if isempty(m.ext[:parameters][:connections][node1][node2])
+                @show(node1,node2)
+                m.ext[:parameters][:connections][node1][node2] = 0
+            end
+        end
+    end
+end
+
+function process_time_series!(m::Model,scenario::String)
+    countries = m.ext[:sets][:countries]
+
+    m.ext[:timeseries] = Dict()
+    m.ext[:timeseries][:demand] = Dict()
+
+    scenario_dict = Dict("Distributed Energy" => "DE","Global Ambition" => "GA","National Trends" => "NT")
+    filename = string(scenario_dict[scenario],"2040_Demand_CY1984.csv")
+    demand_reading = CSV.read(joinpath("Input Data",filename),DataFrame)
+    for country in countries
+        m.ext[:timeseries][:demand][country] = demand_reading[!,country]
+    end
+end
 ##
+
+function build_isolated_model!(m::Model,endtime,VOLL)
+    technologies = m.ext[:sets][:technologies]
+    countries =  m.ext[:sets][:countries]
+    timesteps = collect(1:endtime)
+
+    demand = m.ext[:timeseries][:demand]
+
+    #Variables
+    m.ext[:variables] = Dict()
+    production = m.ext[:variables][:production] = @variable(m,[c= countries, tech=technologies[c],time=timesteps],base_name = "production")
+    load_shedding =  m.ext[:variables][:load_shedding] = @variable(m,[c= countries,time=timesteps],base_name = "production")
+
+    #Expressions
+    m.ext[:expressions] = Dict()
+    total_production_timestep = m.ext[:expressions][:total_production_timestep] =
+        @expression(m, [c = countries, time = timesteps],
+        sum(production[c,tech,time] for tech in technologies[c])
+        )
+
+    production_cost = m.ext[:expressions][:production_cost] =
+        @expression(m, [c = countries, time = timesteps],
+        sum(production[c,tech,time]*100 for tech in technologies[c])
+        )
+    load_shedding_cost = m.ext[:expressions][:load_shedding_cost] =
+        @expression(m, [c = countries, time = timesteps],
+        load_shedding[c,time]*VOLL
+        )
+
+    #Constraints
+    m.ext[:constraints] = Dict()
+    m.ext[:constraints][:demand_met] = @constraint(m,[c = countries, time = timesteps],
+        total_production_timestep[c,time] + load_shedding[c,time]  == demand[c][time]
+    )
+
+    m.ext[:constraints][:production_capacity] = @constraint(m,[c = countries, tech = technologies[c],time = timesteps],
+        production[c,tech,time] == 0
+    )
+
+
+    #Objective
+    m.ext[:objective] = @objective(m,Min, sum(production_cost) + sum(load_shedding_cost))
+
+end
+
 function build_simple_copper_plate_model!(m::Model,endtime,renewable_target)
     #Extract relevant sets
 
@@ -59,7 +216,7 @@ function build_simple_copper_plate_model!(m::Model,endtime,renewable_target)
      @expression(m, [c = countries, tech = technologies[c]], sum(production[c,tech,time] for time in timesteps))
 
     production_cost = m.ext[:expressions][:production_cost] =
-     @expression(m, [c = countries, tech = technologies[c], time = timesteps], endtime/nb_steps*production[c,tech,time]*variable_cost[c][tech])
+     @expression(m, [c = countries, tech = technologies[c], time = timesteps], endtime/8760*production[c,tech,time]*variable_cost[c][tech])
     investment_cost = m.ext[:expressions][:investment_cost] =
      @expression(m,[c=countries, tech = technologies[c]], 1000*installed_capacity[c,tech]*fixed_cost[c][tech])
 
@@ -137,10 +294,8 @@ function build_simple_NTC_model!(m:: Model,endtime,renewable_target)
         total_production_timestep[c,time] - curtailment[c,time] == demand[c][time])
 end
 
-using JuMP
+
 using Gurobi
-using DataFrames
-using CSV
 
 
 reading = CSV.read("Input Data\\gen_cap.csv",DataFrame)
@@ -154,6 +309,72 @@ reading = reading[reading[!,"Climate Year"] .== CY,:]
 country = "BE00"
 reading[reading[!,"Node/Line"] .== country,:Generator_ID]
 
+
+
+reading_lines = CSV.read("Input Data\\lines.csv",DataFrame)
+reading_lines = reading_lines[reading_lines[!,"Scenario"] .== scenario,:]
+reading_lines = reading_lines[reading_lines[!,"Climate Year"] .== CY,:]
+reading_lines = reading_lines[reading_lines[!,"Year"] .== year,:]
+
+
+country = "DE00"
+other_country = "NL00"
+reading_lines = reading_lines[reading_lines[!,"Node1"] .== country,:]
+reading_lines = reading_lines[reading_lines[!,"Node2"] .== other_country,:]
+reading_lines[reading_lines[!,"Parameter"] .== "Export Capacity",:].Value
+
+##
 m = Model(optimizer_with_attributes(Gurobi.Optimizer))
-define_sets(m,"Distributed Energy", 2040,1984)
-m.ext[:sets]
+define_sets!(m,"Distributed Energy", 2040,1984)
+process_parameters!(m,"Distributed Energy", 2040,1984)
+process_time_series!(m,"Distributed Energy")
+build_isolated_model!(m,5,1000)
+optimize!(m)
+
+JuMP.value.(m.ext[:variables][:load_shedding])
+JuMP.value.(m.ext[:variables][:load_shedding]["CY00",1])
+JuMP.value.(m.ext[:expressions][:load_shedding_cost]["CY00",1])
+JuMP.value.(m.ext[:expressions][:total_production_timestep])
+##
+m.ext[:sets][:countries]
+m.ext[:sets][:technologies]["FR00"]
+m.ext[:sets][:dispatchable_technologies]["FR00"]
+
+m.ext[:sets][:connections]
+m.ext[:parameters][:technologies]["BE00"]
+sum(m.ext[:parameters][:technologies]["BE00"][key] for key in keys(m.ext[:parameters][:technologies]["BE00"]))
+
+m.ext[:sets][:connections]["BE00"]
+m.ext[:sets][:connections]["DE00"]
+
+m.ext[:parameters][:connections]
+m.ext[:parameters][:connections]["DE00"]["FR00"]
+m.ext[:parameters][:connections]["LUB1"]["BE00"]
+
+m.ext[:timeseries]
+##
+#Check if all lines from sets are represented in parameters
+for node1 in m.ext[:sets][:countries]
+    ncs = length(m.ext[:sets][:connections][node1])
+    ncp = length(m.ext[:parameters][:connections][node1])
+    @assert(ncs == ncp)
+end
+
+
+#Check if every line has capacity
+for node1 in keys(m.ext[:sets][:connections])
+    @show(node1)
+    for node2 in m.ext[:sets][:connections][node1]
+        print(node2)
+        @assert(!(isempty(m.ext[:parameters][:connections][node1][node2])))
+        @assert(!(isempty(m.ext[:parameters][:connections][node2][node1])))
+        @show(m.ext[:parameters][:connections][node2][node1] == m.ext[:parameters][:connections][node1][node2])
+        @assert(m.ext[:parameters][:connections][node2][node1] == m.ext[:parameters][:connections][node1][node2])
+    end
+end
+m.ext[:parameters][:connections]["NON1"]["NOM1"]
+m.ext[:parameters][:connections]["NOM1"]["NON1"]
+
+(reading_lines[!,"Year"] .== year) .& (reading_lines[!,"Year"] .== year)
+
+reading_lines[reading_lines[!,"Year"] .== year,:]
